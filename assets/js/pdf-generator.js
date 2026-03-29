48eee868128c38855acfbcc2e372aa35fc55af34
Spencer Bliven <spencer.bliven@gmail.com>
2026-03-30 01:04:38 +0200

    fix print link

--- assets/js/pdf-generator.js
+++ assets/js/pdf-generator.js
@@ -1,5 +1,5 @@
 function print() {
-  const printWindow = window.open("/print", "_blank");
+  const printWindow = window.open("print", "_blank");
   printWindow.onload = function () {
     printWindow.print();
     // Close the print window after a delay
